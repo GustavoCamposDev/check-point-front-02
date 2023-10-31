68735760897ea11e77700f43bb9306de0c8a6e0e
GustavoCamposDev <143201440+GustavoCamposDev@users.noreply.github.com>
2023-10-30 22:34:27 -0300

    Add files via upload

---
+++ cp2/animais.js
@@ -0,0 +1,9 @@
+const animals = ["lobo", "guepardo", "salamandra", "formiga", "rato", "leão"];
+ 
+if (!(animals.includes("leão"))){
+    console.log("Leão não existe no array animals.")
+}
+ 
+else {
+    console.log("Existe leão no array animals.")
+}
---
+++ cp2/series.js
@@ -0,0 +1,8 @@
+const my3favoritetvshows = ["Seinfield", "Breaking bad", "The last of us"];
+ 
+if (my3favoritetvshows.length === 3){
+    console.log("true")
+}
+else {
+    console.log("false")
+}
